3f4271dbee9ca3d3126a6f50278ee3ab7e47cb1a
adindaivankamp <adindaivankamaysandaputri@gmail.com>
2023-11-06 16:27:10 +0700

    .

---
+++ ModifikasiDoWhileCuti101.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ModifikasiDoWhileCuti101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do{
+            System.out.println("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
+                    continue;
+                }
+            } else if (konfirmasi.equalsIgnoreCase("t")) {
+                break;
+            }
+            
+        } while (jatahCuti > 0);
+    }
+}
